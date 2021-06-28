4726e23d8efe785680b6d6b11146d0ed60c92d2b
Josh Garretson <josh.garretson@southlakeautomation.com>
2021-06-28 10:10:59 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
